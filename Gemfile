86c9037d570f480b43acef43a55de9bf0b1c7884
SouzaFS <dev.felipesouza@outlook.com>
2024-11-16 13:21:57 -0300

    Added PetModel and Endpoints Access via Swagger

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ end
 
 gem 'rswag'
 gem 'mongoid'
+gem 'rspec-rails'
---
+++ app/models/pet.rb
@@ -0,0 +1,16 @@
+class Pet
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :Breed, type: String
+  field :Energy, type: Integer
+  field :Allegiance, type: Integer
+  field :Intelligence, type: Integer
+  field :Territorial, type: Integer
+  field :Loyalty, type: Integer
+  field :Bonded, type: Integer
+  field :Noisy, type: Integer
+  field :ChildFriendly, type: Integer
+  field :AnimalFriendly, type: Integer
+  field :RaiseDifficulty, type: Integer
+  field :AdultSize, type: Integer
+end
