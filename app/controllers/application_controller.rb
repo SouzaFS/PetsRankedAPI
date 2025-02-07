804338279be1782368ba637faef2b3e4dc7a7d18
SouzaFS <dev.felipesouza@outlook.com>
2025-02-07 02:44:35 -0300

    Realm and Client are Created Automatically in the Beginning of the Container Initialization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,43 @@
 class ApplicationController < ActionController::API
+  
+  <<~COMMENT
+  before_action :authenticate_request
+
+  
+  def authenticate_request
+    
+    @token = request.headers['Authorization']&.split(' ')&.last
+    if @token == nil
+      render json: { error: 'Token Missing' }, status: :unauthorized
+    else
+      @key = Rails.application.credentials.realm_public_key
+      @PUBLIC_KEY = OpenSSL::PKey::RSA.new(@key)
+      @ISSUER = Rails.application.credentials.realm_issuer
+      @AUDIENCE = Rails.application.credentials.client_audience
+
+      begin
+        @decoded_token = JWT.decode(@token, @PUBLIC_KEY, true, {
+          algorithm: 'RS256',
+          iss: @ISSUER,
+          aud: @AUDIENCE,
+          verify_iss: true,
+          verify_aud: true,
+          verify_expiration: true,
+        })
+
+      rescue JWT::ExpiredSignature
+        render json: { error: "Time Expired" }, status: :unauthorized
+      rescue JWT::InvalidIssuerError
+        render json: { error: "Invalid Issuer" }, status: :unauthorized
+      rescue JWT::InvalidAudError
+        render json: { error: "Invalid Audience" }, status: :unauthorized
+        
+      end
+          
+    end
+  
+  end
+
+  COMMENT
+  
 end
